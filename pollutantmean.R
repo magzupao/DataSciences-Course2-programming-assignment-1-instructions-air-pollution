19d0cddd0d38fc05309dc9de2154088819d95be5
magzupao <mguado@gmail.com>
2017-09-17 00:49:31 +0200

    Commit message

---
+++ pollutantmean.R
@@ -0,0 +1,38 @@
+pollutantmean <- function(path.files, pollutant, id = 0) {
+  
+  #We store all file names
+  list.filenames <- list.files(path.files, pattern="*.csv")
+  
+  #numercia list that stores all values, useful to determine the median
+  list.values <- numeric()
+  
+  # this validation is useful if you enter as parameter id = 0
+  if(length(id) == 1){
+    if(id == 0){
+      id = 332
+    }
+  }
+  
+  for(i in c(id) ) {
+    
+    #file name processing
+    filename <- paste(path.files, list.filenames[i], sep = "/")
+    
+    #load the file contents
+    df.File <- read.csv(filename, head=TRUE, sep=",")
+    
+    #filter by sulfate or nitrate
+    if(pollutant == "sulfate"){
+      valores <- df.File$sulfate[!is.na(df.File$sulfate)]
+    }
+    else{
+      valores <- df.File$nitrate[!is.na(df.File$nitrate)]
+    }
+    
+    #It contains all values for each interaction
+    list.values <- append(list.values, valores)
+  }
+  #calculation of the mean
+  return(mean(list.values))
+  
+}
